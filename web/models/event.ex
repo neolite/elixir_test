49d5abbc0ecbaf3ac9a036281dc89215a12ee3dd
Rafkat <ufadiz@gmail.com>
2016-11-20 10:28:34 +0500

    check duration

--- web/models/event.ex
+++ web/models/event.ex
@@ -11,9 +11,6 @@ defmodule ApiTest.Event do
     timestamps()
   end
 
-  @doc """
-  Builds a changeset based on the `struct` and `params`.
-  """
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:title, :start_at, :end_at, :duration])
@@ -21,6 +18,7 @@ defmodule ApiTest.Event do
     |> validate_date_and_duration_presents()
     |> estimate_end_time()
     |> estimate_duration()
+    |> check_duration()
   end
 
   defp validate_date_and_duration_presents(changeset) do
@@ -54,8 +52,21 @@ defmodule ApiTest.Event do
         start_at  = to_datetime get_field(changeset, :start_at)
         end_at    = to_datetime get_field(changeset, :end_at)
         duration  = Timex.Comparable.diff( end_at, start_at, :seconds)
-        put_change(changeset, :duration, duration)
+        changeset
+          |> put_change(:duration, duration)
       confirmation ->
+        changeset
+    end
+  end
+
+  defp check_duration(changeset) do
+   duration = get_field(changeset, :duration)
+    cond do
+      duration < 0 ->
+        changeset |> add_error(:duration, "Incorrect duration. Must be positive")
+      duration = 0 ->
+        changeset
+      duration > 0 ->
         changeset
     end
   end
@@ -63,5 +74,4 @@ defmodule ApiTest.Event do
   defp to_datetime(datetime) do
     datetime |> Ecto.DateTime.to_erl |> Timex.to_datetime("Etc/UTC")
   end
-
 end
